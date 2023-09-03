0526ef0b6e4780ef86886b0cdb91570246158196
unknown <henrytesting8@gmail.com>
2023-09-03 15:14:46 +0700

    JS completed section-9

--- shared.js
+++ shared.js
@@ -4,18 +4,22 @@ var selectPlanButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
 // console.log(backdrop)
 // console.dir(selectPlanButton)
+// console.dir(backdrop.style.backgroundImage)
+// console.dir(backdrop.style['background-image'])
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function () {
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
-    // cara lain
-    // modal.className = "open"; //css mesti ditambahkan juga
-    modal.classList.add('open');
-    backdrop.classList.add('open');
-  });
+if (modal) {
+  for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener("click", function () {
+      // modal.style.display = "block";
+      // backdrop.style.display = "block";
+      // cara lain
+      // modal.className = "open"; //css mesti ditambahkan juga
+      modal.classList.add("open");
+      backdrop.classList.add("open");
+    });
+  }
 }
 
 // backdrop.addEventListener("click", closeModal);
@@ -25,18 +29,22 @@ backdrop.addEventListener("click", function () {
   closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if (modal) {
+  modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
   // modal.style.display = "none";
   // backdrop.style.display = "none";
-  modal.classList.remove('open')
-  backdrop.classList.remove('open')
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function () {
   // mobileNav.style.display = "block";
   // backdrop.style.display = "block";
-  mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
