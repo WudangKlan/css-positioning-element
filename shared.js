020893362e85b874821b8d5b3fcf712e9b834d5d
unknown <henrytesting8@gmail.com>
2023-09-03 14:24:14 +0700

    manipulating element class l-138

--- shared.js
+++ shared.js
@@ -9,21 +9,34 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // cara lain
+    // modal.className = "open"; //css mesti ditambahkan juga
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+// backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
 
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+  modal.classList.remove('open')
+  backdrop.classList.remove('open')
 }
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
-
